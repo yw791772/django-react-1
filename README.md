b3fea990658f32d540fc8939de0305877c295351
Yue Wu <ywu@Shaulitas-MacBook-Air.local>
2019-02-07 10:50:08 -0500

    django + react without using create-react-app

---
+++ README.md
@@ -0,0 +1,1 @@
+This is django + react without using create-react-app
---
+++ project/leads/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from leads.models import Lead
+from leads.serializers import LeadSerializer
+from rest_framework import generics
+
+class LeadListCreate(generics.ListCreateAPIView):
+	queryset = Lead.objects.all()
+	serializer_class = LeadSerializer
